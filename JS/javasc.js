126110c6feed83d477b0680d4eb0c7c2e1aed529
unknown <PC32@MAAPCA02LC0232.duoc.acad>
2024-01-11 13:15:36 -0300

    Commit de funciones y postulación

---
+++ JS/javasc.js
@@ -0,0 +1,17 @@
+function Confirmar() {
+    var salida = document.getElementById("rut").value+"\n" +
+    document.getElementById("mname").value+"\n" +
+    document.getElementById("lname").value+"\n" +
+    document.getElementById("names").value+"\n" +
+    document.getElementById("birth").value+"\n" +
+    document.getElementById("age").value+"\n" +
+    document.getElementById("gender").value+"\n" +
+    document.getElementById("mail").value+"\n" +
+    document.getElementById("phone").value+"\n" +
+    document.getElementById("job").value+"\n" +
+    document.getElementById("moti").value+"\n" ;
+
+    document.getElementById("show").value=salida ;
+
+
+}
